46dab861552298c3001340f5fb2761c663a99447
TheWonderfulLizardOfOz <garancear@gmail.com>
2021-12-18 14:04:18 +0000

    Finished layout

--- GUI/pages.py
+++ GUI/pages.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import os.path
+
 class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
@@ -30,11 +32,17 @@ class AddItemsAndSpellsPage(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
 
+class SettingsPage(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
         self.root = args[0]
         self.root.title("DnD Tools")
+        self.settingsImage = tk.PhotoImage(file = os.path.dirname(__file__) + "/../Images/SettingsImage.png")
+        self.settingsImage = self.settingsImage.subsample(120, 120)
 
         createCharacterPage = CreateCharacterPage(self)
         battlePage = BattlePage(self)
@@ -42,6 +50,7 @@ class MainView(tk.Frame):
         characterSheetPage = CharacterSheetPage(self)
         itemsAndSpellsPage = ItemsAndSpellsPage(self)
         addItemsAndSpellsPage = AddItemsAndSpellsPage(self)
+        settingsPage = SettingsPage(self)
 
         buttonFrame = tk.Frame(self)
         container = tk.Frame(self)
@@ -54,6 +63,7 @@ class MainView(tk.Frame):
         characterSheetPage.place(in_ = container, x = 0, y = 9, relwidth = 1, relheight = 1)
         itemsAndSpellsPage.place(in_ = container, x = 0, y = 9, relwidth = 1, relheight = 1)
         addItemsAndSpellsPage.place(in_ = container, x = 0, y = 9, relwidth = 1, relheight = 1)
+        settingsPage.place(in_ = container, x = 0, y = 9, relwidth = 1, relheight = 1)
 
         buttonCreateCharacterPage = tk.Button(buttonFrame, text = "Create character", command = createCharacterPage.lift)
         buttonBattlePage = tk.Button(buttonFrame, text = "Battle", command = battlePage.lift)
@@ -61,6 +71,7 @@ class MainView(tk.Frame):
         buttonCharacterSheetPage = tk.Button(buttonFrame, text = "View Characters", command = characterSheetPage.lift)
         buttonItemsAndSpellsPage = tk.Button(buttonFrame, text = "Items/Spells", command = itemsAndSpellsPage.lift)
         buttonAddItemsAndSpellsPage = tk.Button(buttonFrame, text = "Add items/spells", command = addItemsAndSpellsPage.lift)
+        settingsButton = tk.Button(buttonFrame, image = self.settingsImage, command = settingsPage.lift)
 
         buttonCreateCharacterPage.pack(side = "left")
         buttonBattlePage.pack(side = "left")
@@ -68,5 +79,6 @@ class MainView(tk.Frame):
         buttonCharacterSheetPage.pack(side = "left")
         buttonItemsAndSpellsPage.pack(side = "left")
         buttonAddItemsAndSpellsPage.pack(side = "left")
+        settingsButton.pack(side = "right")
 
         createCharacterPage.show()
