201adb5d078f65b95c46486584f0e239ffa76d6d
TheWonderfulLizardOfOz <garancear@gmail.com>
2022-02-15 18:48:39 +0000

    Updated to pass input tests

--- objects/race.py
+++ objects/race.py
@@ -5,7 +5,6 @@ import json
 class Races(commands.Cog):
     def __init__(self, bot=None):
         self.bot = bot
-        self.namesList = []
         file = open("races.json")
         self.racesDict = json.load(file)
         file.close()
@@ -17,17 +16,29 @@ class Races(commands.Cog):
 
     @commands.command()
     async def raceDetails(self, ctx, arg):
+        arg = arg.title()
         value = self.racesDict.get(arg)
         if value != None:
             message = self.createMessage(value)
             await ctx.send(message)
         else:
-            await ctx.send("Race not stored")
+            listMessage = ""
+            for item in self.raceList:
+                listMessage += item + ", "
+            listMessage = listMessage[0:-2]
+            await ctx.send("Race not stored\nCurrent available races are: {}".format(listMessage))
 
     def createMessage(self, dict):
         message = ""
         for key, value in dict.items():
-            message += "**`{}:`** {}\n".format(key, value)
+            valueOutput = ""
+            if type(value) is int:
+                valueOutput = "+" + str(value)
+            elif type(value) is list:
+                for item in value:
+                    valueOutput += item + ", "
+                valueOutput = valueOutput[0:-2]
+            message += "**`{}:`** {}\n".format(key.title(), valueOutput)
         return message
 
 def setup(bot):
