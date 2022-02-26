989d4324766276d7e20a96008b40bee1b7d6af95
TheWonderfulLizardOfOz <garancear@gmail.com>
2022-02-26 15:31:54 +0000

    renamed methods

--- objects/owner.py
+++ objects/owner.py
@@ -5,38 +5,38 @@ class Owner(commands.Cog):
         self.bot = bot
 
     #Only runs if user is owner and loads cog
-    @commands.command(name="load")
+    @commands.command()
     @commands.is_owner()
-    async def loadCog(self, ctx, arg):
+    async def load(self, ctx, arg):
         try:
             self.bot.load_extension(arg)
         except Exception as e:
-            await ctx.send(f"**`Error`** {type(e).__name__} - {e}")
+            await ctx.send("**`Error failed to load cog`**")
         else:
-            await ctx.send("**`Success`**")
+            await ctx.send("**`Successfully loaded cog`**")
 
     #Only runs if user is owner and unloads cog meaning that it's commands can't be accessed via discord
-    @commands.command(name = "unload")
+    @commands.command()
     @commands.is_owner()
-    async def unloadCog(self, ctx, arg):
+    async def unload(self, ctx, arg):
         try:
             self.bot.unload_extension(arg)
-        except Exception as e:
-            await ctx.send(f"**`Error`** {type(e).__name__} - {e}")
+        except:
+            await ctx.send("**`Error failed to unload cog`**")
         else:
-            await ctx.send("**`Success`**")
+            await ctx.send("**`Successfully unloaded cog`**")
 
     #Only runs if user is owner and is user to reload a cog which is used to quickly update without having to restart
-    @commands.command(name = "reload")
+    @commands.command()
     @commands.is_owner()
-    async def reloadCog(self, ctx, arg):
+    async def reload(self, ctx, arg):
         try:
             self.bot.unload_extension(arg)
             self.bot.load_extension(arg)
-        except Exception as e:
-            await ctx.send(f"**`Error`** {type(e).__name__} - {e}")
+        except:
+            await ctx.send(f"**`Error failed to relaod cof`**")
         else:
-            await ctx.send("**`Success`**")
+            await ctx.send("**`Successfully reloaded cog`**")
 
 def setup(bot):
     bot.add_cog(Owner(bot))
